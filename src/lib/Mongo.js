2c5f52c48c3b0752b82b86a1d6384150a9e6a45e
Uzair <Uzair318@vt.edu>
2019-12-30 16:00:24 -0500

    added password hashing. need to structure database

--- src/lib/Mongo.js
+++ src/lib/Mongo.js
@@ -6,8 +6,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const MatchModel = require('../models/match');
 const UserModel = require('../models/user');
+const bcrypt = require('bcryptjs');
 
-
+// https://www.npmjs.com/package/bcryptjs
 
 class Mongo {
     constructor() {
@@ -21,24 +22,44 @@ class Mongo {
     }
 
     // define functions here
-    newUser() {
-        return new Promise((resolve, reject) => {
-            var newUser = new UserModel({
-                username: 'exampleUserName',
-                passwordHash: 'examplePasswordHash',
-                created: new Date()
-            })
 
-            newUser.save()
+    /**
+     * Creates a new user in the database
+     * @param {String} userName to store
+     * @param {String} password password to hash and store
+     */
+    newUser(userName, password) {
+        return new Promise((resolve, reject) => {
+            bcrypt.hash(password, 10, (err, hash) => {
+
+                //store hash in db
+                var newUser = new UserModel({
+                username: userName,
+                passwordHash: hash,
+                created: new Date()
+                })
+
+                newUser.save()
                 .then(doc => {
-                    console.log('New user saved to doc: ' + doc);
+                    console.log('New user ' + userName +  ' saved to doc: ' + doc);
                 })
                 .catch(err => {
                     console.error(err);
                 })
+            })
+        })
+    }
+                    
+    /**
+     * Retrieves a user from the DB (verification elsewhere)
+     * @param {String} userName to retrieve
+     */
+    getUser(userName) {
+        return new Promise((resolve, reject) => {
 
         })
     }
+
 
 }
 
