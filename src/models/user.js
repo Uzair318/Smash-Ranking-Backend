61a98a7e051daab14a464519661b6c8267a4a678
Uzair <Uzair318@vt.edu>
2020-01-07 22:14:28 -0500

    added rating number and games played fields to user model for elo algorithm

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,14 @@
 // schema for user
 const mongoose = require('mongoose');
+const matchModel = require('./match')
 
 var userSchema = mongoose.Schema({
     username: {type: String, required: true, unique: true},
     passwordHash: {type: String, required: true},
     created: {type: Date, default: () => new Date()},
+    rating_number: {type: Number, required: true},
+    games_played: {type: Number, required: true}
+    // last match played??
 });
 
 module.exports = mongoose.model('user', userSchema);
